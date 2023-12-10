3ac83ac3cf09678d145e45684b9cfd2f57f9aa08
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-12-10 19:46:11 +0100

    feat: header style

--- admin/header.php
+++ admin/header.php
@@ -3,4 +3,10 @@
         <img src="<?=  plugin_dir_url(__DIR__) ?>assets/images/header_logo_ekhos.png" alt="ID SOUND">
         <h1>ID SOUND</h1>
     </div>
+    <nav class="idsheader_navigation">
+        <a href="#linked">Liées des sons</a>
+        <a href="#characters">Personnages</a>
+        <a href="#sound">Sons</a>
+        <a href="#settings">Paramètres</a>
+    </nav>
 </div>

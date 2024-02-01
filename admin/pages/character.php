fdf57aedf7912324bddf330a0666653fcd4d3c06
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2024-02-01 14:15:21 +0100

    feat: add and delete sound character v2

--- admin/pages/character.php
+++ admin/pages/character.php
@@ -50,6 +50,8 @@
                 this.sounds = r.items;
             },
         });
+
+        /* add character */
         Alpine.store('modal_new', {
             isLoading: false,
             name: '',
@@ -69,6 +71,8 @@
                 this.sound = 'null';
             },
         });
+
+        /* update character */
         Alpine.store('modal_update', {
             isLoading: false,
 
@@ -84,6 +88,8 @@
                 document.querySelector(`#idscharacter_modal_update`).close();
             },
         });
+
+        /* delete character */
         Alpine.store('modal_delete', {
             isLoading: false,
 
@@ -96,6 +102,26 @@
                 this.isLoading = false;
                 base.getItems();
                 document.querySelector(`#idscharacter_modal_delete`).close();
+            },
+        });
+
+        /* update & delete audio */
+        Alpine.store('modal_update_sound', {
+            isLoading: false,
+            sound: 'null',
+
+            async request() {
+                const base = Alpine.store('base');
+                this.isLoading = true;
+                const r = await base.request(`update/${base.selected.id}`, {
+                    name: base.selected.name,
+                    sound: this.sound
+                });
+                this.isLoading = false;
+                base.getItems();
+                document.querySelector(`#idscharacter_modal_add_audio`).close();
+                document.querySelector(`#idscharacter_modal_delete_audio`).close();
+                this.sound = 'null';
             },
         });
     });
@@ -212,14 +238,18 @@
                 </form>
                 <div class="container flex flex-col gap-3">
                     <h3 class="font-bold text-lg">Lier un son</h3>
-                    <select class="select select-bordered input-sm max-w-full-select">
-                        <option disabled selected>Who shot first?</option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
+                    <select x-data x-model="$store.modal_update_sound.sound"
+                            class="select select-bordered input-sm max-w-full-select">
+                        <option disabled value="null">Aucun</option>
+                        <template x-data x-for="(item, index) in $store.base.sounds" :key="index">
+                            <option x-bind:value="item.id" x-text="item.name"></option>
+                        </template>
                     </select>
-                    <button class="btn btn-info mt-2 gap-0">
-                        <span class="loading loading-spinner hidden"></span>
-                        <span>Lier un son</span>
+                    <button x-data class="btn btn-info mt-2 gap-0"
+                            x-on:click="$store.modal_update_sound.request()">
+                        <span x-data class="loading loading-spinner hidden"
+                              :class="{ 'hidden': !$store.modal_update_sound.isLoading }"></span>
+                        <span x-data :class="{ 'hidden': $store.modal_update_sound.isLoading }">Lier un son</span>
                     </button>
                 </div>
             </div>
@@ -236,9 +266,11 @@
                 </form>
                 <div class="container flex flex-col gap-3">
                     <h3 class="font-bold text-lg">Supprimer la liaison de ce son ?</h3>
-                    <button class="btn btn-error mt-2 gap-0">
-                        <span class="loading loading-spinner hidden"></span>
-                        <span>Supprimer ce son</span>
+                    <button x-data class="btn btn-error mt-2 gap-0"
+                            x-on:click="$store.modal_update_sound.request()">
+                        <span x-data class="loading loading-spinner hidden"
+                              :class="{ 'hidden': !$store.modal_update_sound.isLoading }"></span>
+                        <span x-data :class="{ 'hidden': $store.modal_update_sound.isLoading }">Supprimer ce son</span>
                     </button>
                 </div>
             </div>
@@ -283,15 +315,23 @@
                     <!-- AUDIO -->
                     <td class="flex items-center gap-2">
                         <audio controls :src="item.sound_url" :class="{'hidden': !item.sound_url}"></audio>
-                        <button class="btn btn-circle btn-neutral" :class="{'hidden': !item.sound_url}"
-                                onclick="idscharacter_modal_delete_audio.showModal()">
+                        <!-- delete button -->
+                        <button class="btn btn-circle btn-neutral"
+                                :class="{'hidden': !item.sound_url}"
+                                onclick="idscharacter_modal_delete_audio.showModal()"
+                                @click="$store.base.updateSelected(
+                                item.id, item.name, 'null')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                  class="bi bi-trash3" viewBox="0 0 16 16">
                                 <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                             </svg>
                         </button>
-                        <button class="btn btn-circle btn-neutral" :class="{'hidden': item.sound_url}"
-                                onclick="idscharacter_modal_add_audio.showModal()">
+                        <!-- add button -->
+                        <button class="btn btn-circle btn-neutral"
+                                :class="{'hidden': item.sound_url}"
+                                onclick="idscharacter_modal_add_audio.showModal()"
+                                @click="$store.base.updateSelected(
+                                item.id, item.name, item.sound_id)">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                  class="bi bi-plus" viewBox="0 0 16 16">
                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
